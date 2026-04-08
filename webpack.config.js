4e661c03dd58f6d38fdc68a261b1a2c58b78352d
Ksenia Grom <gca-2001@mail.ru>
2026-04-08 22:35:51 +0300

    Correct webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,49 +8,35 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     clean: true,
-    publicPath: './', 
+    publicPath: './',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        use: 'babel-loader',
       },
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-        ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'images/[name][ext]', 
+          filename: 'images/[name][ext]',
         },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html', 
+      template: './index.html', 
       filename: 'index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: 'style.css', 
+      filename: 'style.css',
     }),
   ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
-    },
-    compress: true,
-    port: 9000,
-    hot: true,
-    open: true,
-  },
 };
